ca1a00828d5071c7bb8dbca2d34b107471bfd1ab
kunboy1608 <71431145+kunboy1608@users.noreply.github.com>
2021-07-05 15:47:31 +0700

    CompleteApp
    ---Thanks---
    - HDP (me)
    - PQH
    - DHC
    - TNTD
    - VQT

---
+++ PTTK_HQTCSDL_v2.0/src/org/resources/IconUtilities.java
@@ -0,0 +1,26 @@
+package org.resources;
+
+import java.awt.Color;
+import javax.swing.Icon;
+
+public class IconUtilities {
+
+    // MainFrame Icons
+    public static final Icon ICON_BILL = MetroUI.getGoogleIcon(MetroUI.icons.STYLE, 25, Color.WHITE);
+    public static final Icon ICON_FEATURES = MetroUI.getGoogleIcon(MetroUI.icons.REORDER, 25, Color.WHITE);
+    public static final Icon ICON_STUDENT = MetroUI.getGoogleIcon(MetroUI.icons.PEOPLE, 25, Color.WHITE);
+    public static final Icon ICON_STAFF = MetroUI.getGoogleIcon(MetroUI.icons.VPN_KEY, 25, Color.WHITE);
+    public static final Icon ICON_ROOM = MetroUI.getGoogleIcon(MetroUI.icons.LAYERS, 25, Color.WHITE);
+    public static final Icon ICON_STATISTIC = MetroUI.getGoogleIcon(MetroUI.icons.EQUALIZER, 25, Color.WHITE);
+    public static final Icon ICON_DEPARTMENT = MetroUI.getGoogleIcon(MetroUI.icons.FOLDER_SHARED, 25, Color.WHITE);
+    public static final Icon ICON_UNIVERSITY = MetroUI.getGoogleIcon(MetroUI.icons.LOCATION_CITY, 25, Color.WHITE);
+    public static final Icon ICON_BUILDING = MetroUI.getGoogleIcon(MetroUI.icons.BUSINESS, 25, Color.WHITE);
+
+    // Panel Icons
+    public static final Icon ICON_ADD = MetroUI.getGoogleIcon(MetroUI.icons.ADD_CIRCLE_OUTLINE, 25, Color.BLACK);
+    public static final Icon ICON_REMOVE = MetroUI.getGoogleIcon(MetroUI.icons.REMOVE_CIRCLE_OUTLINE, 25, Color.RED);
+    public static final Icon ICON_EDIT = MetroUI.getGoogleIcon(MetroUI.icons.CREATE, 25, Color.BLACK);
+    public static final Icon ICON_FIND = MetroUI.getGoogleIcon(MetroUI.icons.SEARCH, 25, Color.BLACK);
+    public static final Icon ICON_LOAD = MetroUI.getGoogleIcon(MetroUI.icons.REFRESH, 25, Color.BLACK);
+    public static final Icon ICON_PRINT = MetroUI.getGoogleIcon(MetroUI.icons.PRINT, 25, Color.BLACK);
+}
---
+++ PTTK_HQTCSDL_v2.0/src/org/resources/MetroUI.java
@@ -0,0 +1,217 @@
+package org.resources;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.font.TextAttribute;
+import java.util.Collections;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.plaf.metal.MetalButtonUI;
+import javax.swing.plaf.metal.MetalTextFieldUI;
+import jiconfont.IconFontSwing;
+import jiconfont.icons.GoogleMaterialDesignIcons;
+
+public class MetroUI {
+    
+    public static final Color COLOR_WHITE = Color.WHITE;
+    public static final Color COLOR_BACKGROUND = new Color(51, 51, 51);
+    public static final Color COLOR_BACKGROUND_SELECTED = Color.GRAY;
+    
+    public static final Insets metroMargin = new Insets(0, 10, 0, 10);
+    public static final Border selectedBorder = MetroUI.getBorder(Color.WHITE, COLOR_BACKGROUND, 3, 10  );
+    public static final Border unselectedBorder = MetroUI.getBorder(COLOR_BACKGROUND, COLOR_BACKGROUND, 0, 13);
+    public static final Border hoverBorder = MetroUI.getBorder(Color.GRAY, COLOR_BACKGROUND, 3, 10);
+    public static final Border buttonBorder = MetroUI.getBorder(Color.BLACK, Color.WHITE, 2, 0);
+    public static final Border textFieldBorder = MetroUI.getBorder(Color.BLACK, Color.WHITE, 1, 10);
+    public static final Border voidBorder = MetroUI.getBorder(COLOR_WHITE, Color.WHITE, 0, 0);
+    
+    public static GoogleMaterialDesignIcons icons;
+    
+    public static void notificate(String title, 
+                                    String message,
+                                    TrayIcon.MessageType type) {
+        SystemTray sysTray = SystemTray.getSystemTray();
+        //setting the icon
+        TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().createImage("src/org/resources/img/icon.jpeg"), "jpg");
+        trayIcon.setImageAutoSize(true);
+        try {
+            //setting and display the notification
+            sysTray.add(trayIcon);
+        } catch (AWTException ex){
+            ex.printStackTrace();
+        }
+        trayIcon.displayMessage(title, message, type);
+    }
+            
+    public static Border getBorder(Color lineColor, Color paddingColor, int width, int padding) {
+        return BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(lineColor, width),
+                    BorderFactory.createLineBorder(paddingColor, padding)
+               );
+    }
+    
+    public static Border getBorder(Color lineColor, int width, int padding) {
+        return BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(lineColor, width),
+                    BorderFactory.createEmptyBorder(1, padding, 1, padding)
+        );
+    }
+    
+    public static void applySpecilized(JButton b) {
+        Dimension bd = b.getSize();
+        Point bp = b.getLocation();
+        
+        b.setBackground(COLOR_BACKGROUND);
+        b.setForeground(COLOR_WHITE);
+        b.setBorder(voidBorder);
+        b.setContentAreaFilled(true);
+        b.setUI(new MetalButtonUI() { });
+        b.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) { }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                b.setBackground(COLOR_BACKGROUND_SELECTED);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                b.setBackground(COLOR_BACKGROUND);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) { }
+
+            @Override
+            public void mouseExited(MouseEvent me) { }  
+        });
+    }
+    
+    public static void apply(JToggleButton b) {
+        b.setBorder(null);
+        b.setHorizontalAlignment(SwingConstants.LEFT);
+        b.setMargin(metroMargin);
+        b.setUI(new MetalButtonUI() {
+            @Override
+            protected Color getSelectColor() {
+                return new Color(51, 51, 51);
+            }
+        }
+        );
+        if (b.isSelected()) {
+            b.setFont(b.getFont().deriveFont(
+                            Collections.singletonMap(TextAttribute.WEIGHT,  TextAttribute.WEIGHT_BOLD)
+                    ));
+            b.setBorder(selectedBorder);
+        }
+        b.addChangeListener((ChangeEvent evt) -> {
+            if (b.isSelected()) {
+                b.setFont(b.getFont().deriveFont(
+                        Collections.singletonMap(TextAttribute.WEIGHT,  TextAttribute.WEIGHT_BOLD)
+                ));
+                b.setBorder(selectedBorder);
+            } else {
+                b.setFont(b.getFont().deriveFont(
+                        Collections.singletonMap(TextAttribute.WEIGHT,  TextAttribute.WEIGHT_MEDIUM)
+                ));
+                b.setBorder(unselectedBorder);
+            }
+        });
+        b.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {}
+
+            @Override
+            public void mousePressed(MouseEvent me) {}
+
+            @Override
+            public void mouseReleased(MouseEvent me) {}
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                if (!b.isSelected()) {
+                    b.setBorder(hoverBorder);
+                }
+            }
+            @Override
+            public void mouseExited(MouseEvent me) {
+                if (!b.isSelected()) {
+                    b.setBorder(unselectedBorder);
+                }
+            }
+            
+        });
+    }
+    public static void apply(JButton b) {
+
+        b.setBackground(Color.WHITE);
+        b.setBorder(buttonBorder);
+        b.setContentAreaFilled(true);
+        b.setUI(new MetalButtonUI() { });
+        b.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) { }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                b.setBackground(Color.GRAY);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                b.setBackground(Color.WHITE);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) { }
+
+            @Override
+            public void mouseExited(MouseEvent me) { }
+            
+        });
+    }   
+    public static void apply(JTextField tf, String placeHolder) {
+        tf.setUI(new MetalTextFieldUI());
+        tf.setBorder(textFieldBorder);
+        tf.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent fe) {
+                if (tf.getText().equals(placeHolder)) {
+                    tf.setText("");
+                    tf.setForeground(Color.BLACK);
+                } 
+            }
+
+            @Override
+            public void focusLost(FocusEvent fe) {
+                if (tf.getText().equals("")) {
+                     tf.setText(placeHolder);
+                    tf.setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+ 
+    public static Icon getGoogleIcon(GoogleMaterialDesignIcons iconStyle, int size, Color color) {
+        IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+        return IconFontSwing.buildIcon(iconStyle, size, color);
+    }
+    
+}
